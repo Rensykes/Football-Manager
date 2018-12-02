e36bf887562c8fb9be268f5952ac2657a1b0396b
universal09 <chris.l9@libero.it>
2018-12-02 18:55:59 +0100

    Team details filled

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 //import { RoutingModule} from 'src/app/routing/routing.module'
 import { MatchListComponent } from "src/app/match-list/match-list.component";
 import { TeamDetailsComponent } from 'src/app/team-details/team-details.component'
+import {PlayerDetailsComponent} from 'src/app/player-details/player-details.component'
 
 
 const routes: Routes = [{
@@ -13,6 +14,10 @@ const routes: Routes = [{
   path: "",
   redirectTo: "/matches",
   pathMatch: "full"
+},
+{
+  path: "player/:id",
+  component: PlayerDetailsComponent
 },
 {
   path: "team/:id",
--- src/app/player-details/player-details.component.ts
+++ src/app/player-details/player-details.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchdataService } from '../fetchdata.service';
+import { Observable } from 'rxjs/Observable';
+import { HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-player-details',
   templateUrl: './player-details.component.html',
-  styleUrls: ['./player-details.component.css']
+  styleUrls: ['./player-details.component.css'],
+  providers: [FetchdataService]
 })
 export class PlayerDetailsComponent implements OnInit {
+  player={};
+  header = new HttpHeaders({'X-Auth-Token':'aa89ef54a73b4df6a2e389906426b90b'})
+  Url = "https://api.football-data.org/v2/players";
+  
+  constructor(private srv: FetchdataService,  private route: ActivatedRoute) { }
 
-  constructor() { }
-
+  getPosts() : void {
+    const id = this.route.snapshot.paramMap.get("id");
+    const urlReq = `${this.Url}/${id}`;
+    this.srv.getData(urlReq)
+    
+      .subscribe(
+        data => {
+          this.player = data;
+          console.log(data);
+        },
+        error=> console.log(error)
+      )
+      
+    }
+  
   ngOnInit() {
+    this.getPosts();
   }
 
 }
