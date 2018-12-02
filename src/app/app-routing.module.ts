4665213e109150cca9d60e948f3ff44a1cdc3591
Francesco Lacriola <chiccodallacinacolfurgoneyahoo.it>
2018-12-02 16:53:19 +0100

    Added Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import { RoutingModule} from 'src/app/routing/routing.module'
+import { MatchListComponent } from "src/app/match-list/match-list.component"
 
-const routes: Routes = [];
+
+const routes: Routes = [{
+  path: "matches",
+  component: MatchListComponent
+},
+{
+  path: "",
+  redirectTo: "/matches",
+  pathMatch: "full"
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,28 @@ import { FormsModule } from '@angular/forms';
  // HttpModule
 import {HttpClientModule} from '@angular/common/http';
 import {TitleComponent} from './title.component';
+import { AppRoutingModule} from 'src/app/app-routing.module';
+
 
 import { AppComponent } from './app.component';
 import { MatchListComponent } from './match-list/match-list.component';
+import { TeamDetailsComponent } from './team-details/team-details.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
-    MatchListComponent
+    MatchListComponent,
+    TeamDetailsComponent
   
   ],
   imports: [
     BrowserModule,
     // HttpModule
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
    
   ],
   providers: [],
